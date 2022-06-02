e50c3a208537cd172f0deac3340a2f58ad34260a
Jake Torrance <jaket1234@hotmail.com>
2022-06-02 18:24:24 +0100

    Scripting

--- 03_Scripting/solutions/concat_two_strings.sh
+++ 03_Scripting/solutions/concat_two_strings.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$#" -ne "2" ]; then
+    exit 1
+fi
+
 RESULT="$1$2"
 
 echo -n "$RESULT"
--- 03_Scripting/test_3.sh
+++ 03_Scripting/test_3.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bats
 
 @test "Concat two strings doesn't support anything other than 2 arguments" {
-    ./concat_two_strings onlyone
-    [ "$?" -neq 0 ]
+    ./concat_two_strings.sh onlyone && exit 1
 }
